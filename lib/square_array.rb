47c2a9c72b1d63320e097813ff7af3164dc7975a
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-08 00:39:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,5 @@
 def square_array(array)
-  counter = 0
-  
-  while counter > array.lenght do
-  puts array[counter] 
-  couter += 1
-  
-  count = array ** 2
-  
-  puts count
+   array.each( array ** 2)
   
 end
 end
